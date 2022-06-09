507adac3681ad9f498f00dd1d17a6bec555dba2c
Md. Abdullah Al Mamun <58597051+abdullahIt18040@users.noreply.github.com>
2022-06-10 00:53:57 +0600

    add name

--- hello.cpp
+++ hello.cpp
@@ -5,5 +5,10 @@ using namespace std;
 
      cout<<" my name is abdullah al mamun"<<endl;
      cout<<" my name is abdullah al mamun"<<endl;;
-     cout<<" my name is abdullah al mamun"<<endl;;
+     cout<<" my name is abdullah al mamun"<<endl;
+     int n;
+     cin>>n;
+     cout<<n;
+     cout<<" my university name is  MBSTU"<<endl;
+         cout<<" my dept name name is  ict "<<endl;
  }
